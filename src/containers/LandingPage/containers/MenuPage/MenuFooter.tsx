c2e001aad20aa3ef9873f04eb9ad1a9d44622d27
ALABOET <kennynotch11@gmail.com>
2023-01-31 13:11:54 +0300

    Landing-7: Optimisation

--- src/containers/LandingPage/containers/MenuPage/MenuFooter.tsx
+++ src/containers/LandingPage/containers/MenuPage/MenuFooter.tsx
@@ -5,7 +5,7 @@ const MenuFooter:FC = () => {
   return (
     <div className={classNames.footer}>
       <div className={classNames.footer__year} style={{ color: '#FFFFFF'}}>2022</div>
-      <div className={classNames.footer__onepix}>Разработано Onepix</div>
+      <div className={classNames.footer__onepix}>Разработано ALABOET</div>
       <div className={classNames.footer__rights} style={{ color: '#FFFFFF'}}>Все права защищены</div>
     </div>
   )
